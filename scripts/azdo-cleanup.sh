4c247348aa2d57cb8d7830fb4c9846ac4f4d469b
jldeen <jessde@microsoft.com>
2019-02-19 19:56:31 +0800

    curl fixes

--- scripts/azdo-cleanup.sh
+++ scripts/azdo-cleanup.sh
@@ -2,6 +2,6 @@
 set -eou pipefail
 source ./scripts/variables.sh
 
-curl -u $(presenter):$(PAT) --request DELETE https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?api-version=5.0&comment=delete&forceDelete='true' && echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted."
+curl -u $(presenter):$(PAT) --request DELETE "https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?forceDelete=true&api-version=5.0" && echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted."
 
-curl -u $(presenter):$(PAT) --request DELETE https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0 && echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted."
+curl -u $(presenter):$(PAT) --request DELETE "https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0" && echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted."
