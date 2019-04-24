ef0ee10c7437b1a71be8aba0d969c8ddfdeb0b02
jldeen <jedeen@outlook.com>
2019-04-24 10:06:34 +0200

    udpate cleanup

--- scripts/azdo-cleanup.sh
+++ scripts/azdo-cleanup.sh
@@ -2,6 +2,11 @@
 set -eou pipefail
 source ./scripts/variables.sh
 
-curl -u $(presenter):$(PAT) --request DELETE "https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?forceDelete=true&api-version=5.0" && echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted."
-sleep 10
-curl -u $(presenter):$(PAT) --request DELETE "https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0" && echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted."
+releaseAPI="https://vsrm.dev.azure.com/$(organization)/$(project)/_apis/release/definitions/$(releaseDef)?forceDelete=true&api-version=5.0"
+buildAPI="https://dev.azure.com/$(organization)/$(project)/_apis/build/definitions/$(buildDef)?api-version=5.0"
+
+curl -s -u $(presenter):$(PAT) --request DELETE $releaseAPI > /dev/null && curl -s -u $(presenter):$(PAT) --request GET $releaseAPI | grep 'does not exist' > /dev/null && echo "$(organization) $(project) Release Definition $(releaseDef) has been successfully deleted." || echo "Release definition $(releaseDef) not deleted. Please check your inputs, including your PAT, and try again."
+
+sleep 5
+
+curl -s -u $(presenter):$(PAT) --request DELETE $buildAPI > /dev/null && curl -s -u $(presenter):$(PAT) --request GET $buildAPI | grep 'was not found' > /dev/null && echo "$(organization) $(project) Build Definition $(buildDef) has been successfully deleted." || echo "Build definition $(buildDef) not deleted. Please check your inputs, including your PAT, and try again."
